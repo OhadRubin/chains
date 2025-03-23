41e44d12e62962e077d44a0f96e6ed2055953fec
OhadRubin <ohadr@mail.tau.ac.il>
2025-03-23 17:15:21 +0200

    ...

--- src/msg_chain.py
+++ src/msg_chain.py
@@ -6,13 +6,13 @@ class MessageChain:
     @staticmethod
     def get_chain( model: str, **kwargs):
         if "claude" in model:
-            from src.chains.claude_msg_chain import ClaudeMessageChain
+            from src.msg_chains.claude_msg_chain import ClaudeMessageChain
             return ClaudeMessageChain(**kwargs)
         elif "gemini" in model:
-            from src.chains.gemini_msg_chain import GeminiMessageChain
+            from src.msg_chains.gemini_msg_chain import GeminiMessageChain
             return GeminiMessageChain(**kwargs)
         elif "gpt" in model:
-            from src.chains.oai_msg_chain import OpenAIMessageChain
+            from src.msg_chains.oai_msg_chain import OpenAIMessageChain
             return OpenAIMessageChain(**kwargs)
         else:
             raise ValueError(f"Model {model} not supported")
